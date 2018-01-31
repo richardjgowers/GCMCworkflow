a6b1d9c57be9371cc1b27a7df7f4c68479dc82b4
Richard Gowers <richardjgowers@gmail.com>
2018-01-31 11:45:28 +0000

    use only a single value for version

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 import os
+import re
 from setuptools import setup, find_packages
 
 
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst'), 'r') as f:
     long_description = f.read()
+with open(os.path.join(here, 'gcmcworkflow', '__init__.py'), 'r') as f:
+    version = re.search("'(\d+\.\d+\.\d+)'", f.read()).groups()[0]
 
 setup(
     name='GCMCworkflow',
-    version='0.0.1',
+    version=version,
     long_description=long_description,
 
     packages=find_packages(),
