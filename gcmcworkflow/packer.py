b8c2a5062185b5eb1e5a3cc3cf10eb421c9b023a
richardjgowers <richardjgowers@gmail.com>
2018-04-28 21:20:51 -0400

    more work on packer

--- gcmcworkflow/packer.py
+++ gcmcworkflow/packer.py
@@ -6,21 +6,23 @@ Analyse
 CheckFlat
 
 """
+import itertools
 import fireworks as fw
 from fireworks.utilities.fw_utilities import explicit_serialize as xs
 import numpy as np
 
 from . import firetasks
+from . import utils
 from .workflow_creator import make_sampling_point
 
 
 @xs
 class CapacityDecider(fw.FiretaskBase):
-    required_params = ['nparallel']
+    required_params = ['fmt', 'workdir']
     optional_params = ['previous_results']
 
     @staticmethod
-    def make_detour(nparallel, previous):
+    def make_detour(previous, wfname, fmt, template, workdir, simhash):
         # make more simulations at higher pressures
         temperature = max(r[0] for r in previous)
         max_pressure = max(r[1] for r in previous)
@@ -36,18 +38,31 @@ class CapacityDecider(fw.FiretaskBase):
                 pressure=P,
                 ncycles=None,
                 nparallel=1,
-                fmt=self['fmt'],
+                fmt=fmt,
                 wfname=wfname,
                 template=template,
-                workdir=self['workdir'],
+                workdir=workdir,
                 simple=False,
                 simhash=simhash,
             )
+            new_sims.extend(runs)
+            new_pps.append(pps)
 
         new_me = fw.Firework(
-            [self.__class__(nparallel=nparallel, previous_results=previous)],
+            [self.__class__(
+                fmt=fmt,
+                workdir=workdir,
+                previous_results=previous,
+            )],
             parents=pps,
+            spec={
+                '_category': wfname,
+                'template': template,
+                'simhash': simhash,
+            },
         )
+
+        return fw.Workflow(new_sims + new_pps + [new_me])
 
     @staticmethod
     def decide_if_flat(results):
@@ -59,8 +74,7 @@ class CapacityDecider(fw.FiretaskBase):
           True if flat
         """
         # sort by pressure
-        results = sorted(results, key=lambda x: x[1])
-        vals = np.array([r[2] for r in results[-3:]])
+        vals = np.array([r[2] for r in results])
         mean = vals.mean()
 
         # are all values within 5% of the mean?
@@ -68,35 +82,37 @@ class CapacityDecider(fw.FiretaskBase):
 
     def run_rask(self, fw_spec):
         # results are given as (T, P, mean, std, g)
-        results = (fw_spec['results_array'] +
-                   self.get('previous_results', []))
+        total_results = (fw_spec['results_array'] +
+                         self.get('previous_results', []))
 
-        finished = self.decide_if_flat(results)
+        finished = self.decide_if_flat(fw_spec['results_array'])
 
         if not finished:
             # issue detour with more pressures
             return fw.FWAction(
                 detours=self.make_detour(
-                    previous=results,
-                    nparallel=self['nparallel'],
-                )
+                    previous=total_results,
+                    wfname=fw_spec['_category'],
+                    fmt=self['fmt'],
+                    template=fw_spec['template'],
+                    workdir=self['workdir'],
+                    simhash=fw_spec['simhash'],
+                 )
             )
         else:
             # continue to IsothermCreate
             return fw.FWAction(
-                update_spec={'results_array': results}
+                update_spec={'results_array': total_results}
             )
 
 
-def make_packing_workflow(spec, simple=True):
+def make_capacity_measurement(spec):
     """Create an entire Isotherm creation Workflow
 
     Parameters
     ----------
     spec : dict
       has all the information for workflow
-    simple : bool, optional
-      use decorrelation analysis to determine if to run more sims
 
     Returns
     -------
@@ -112,61 +128,64 @@ def make_packing_workflow(spec, simple=True):
     wfname = spec['name']
 
     if not isinstance(template, dict):
-        dict_template = False
         # Passed path to template
         # if passed path to template, slurp it up
 
         # old method of slurping up directory
-        stuff = utils.slurp_directory(template)
+        stuff = None
+        slurped = utils.slurp_directory(template)
+        simfmt = utils.guess_format(slurped)
     else:
-        dict_template = True
         # Else passed dict of stuff
         stuff = template
+        simfmt = utils.guess_format(stuff)
+        stuff = utils.escape_template(stuff)
 
-    simfmt = utils.guess_format(stuff)
-    stuff = utils.escape_template(stuff)
-
-    if dict_template:
-        init = fw.Firework(
-            firetasks.InitTemplate(contents=stuff, workdir=workdir),
-            spec={'_category': wfname},
-            name='Template Init'
-        )
-        setup = [init]
-    else:
-        init = None
-        setup = []
-
-    simulations = []  # list of simulation fireworks
-    post_processing = []  # list of post processing fireworks
-    for T, P in itertools.product(temperatures, pressures):
-        this_condition = make_Simfireworks(
-            parent_fw=init,
-            T=T, P=P, ncycles=ncycles, nparallel=nparallel,
-            simfmt=simfmt, wfname=wfname,
-            template=template, workdir=workdir,
-        )
-        this_condition_PP = make_Analyse(
-            parent_fw=this_condition,
-            T=T, P=P,
-            wfname=wfname,
-            simple=simple,
-        )
-
-        simulations.extend(this_condition)
-        post_processing.append(this_condition_PP)
-
-    flat_decide = fw.Firework(
-        CapacityDecider(),
+    init = fw.Firework(
+        [firetasks.InitTemplate(contents=stuff, workdir=workdir),
+         firetasks.CreatePassport(workdir=workdir)],
+        spec={
+            '_category': wfname,
+            'template': template,
+        },
+        name='Template Init',
     )
 
-    iso_create = fw.Firework(firetasks.IsothermCreate(workdir=workdir),
-                             parents=post_processing,
-                             spec={'_category': wfname},
-                             name='Isotherm create')
+    simulation_steps = []  # list of simulation fireworks
+    analysis_steps = []  # list of post processing fireworks
+    for T, P in itertools.product(temperatures, pressures):
+        this_condition, this_condition_analysis = make_sampling_point(
+            parent_fw=init,
+            temperature=T,
+            pressure=P,
+            ncycles=ncycles,
+            nparallel=nparallel,
+            fmt=simfmt,
+            wfname=wfname,
+            template=template,
+            workdir=workdir,
+            simple=False,
+        )
+        simulation_steps.extend(this_condition)
+        analysis_steps.append(this_condition_analysis)
+
+    capacity = fw.Firework(
+        [CapacityDecider(fmt=simfmt, workdir=workdir)],
+        # pass init to give template in spec
+        parents=[init] + analysis_steps,
+        spec={'_category': wfname},
+        name='Capacity decider',
+    )
+
+    iso_create = fw.Firework(
+        [firetasks.IsothermCreate(workdir=workdir)],
+        parents=[capacity],
+        spec={'_category': wfname},
+        name='Isotherm create',
+    )
 
     wf = fw.Workflow(
-        setup + simulations + post_processing + [iso_create],
+        [init] + simulation_steps + analysis_steps + [capacity, iso_create],
         name=wfname,
         metadata={'GCMCWorkflow': True},  # tag as GCMCWorkflow workflow
     )
